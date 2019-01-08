0918b1f902e2fb4ebb4d0055779442fbc753fd1b
Miguel Bolivar <MBolivar360@gmail.com>
2019-01-08 14:34:30 -0400

    Playing around with variable declaration, static type assignation and inferred types declaration

---
+++ cards/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades"
+	card = "Five of Diamonds"
+	fmt.Println(card)
+}
+
+func newCard() {
+	
+}
