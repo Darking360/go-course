f99ad1627c631f897ee35818f9f64c0ccb4e5e77
Miguel Bolivar <MBolivar360@gmail.com>
2019-01-14 14:26:15 -0500

    Adding and deleting properties

--- map/main.go
+++ map/main.go
@@ -3,9 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "ff0000",
-		"green": "4bf745",
-	}
+	colors := make(map[int]string)
+	// var colors map[string]string
+	// colors := map[string]string{
+	//	"red":   "ff0000",
+	//	"green": "4bf745",
+	//}
+	colors[10] = "#ffffff"
+
+	delete(colors, 10)
+
 	fmt.Println(colors)
 }
