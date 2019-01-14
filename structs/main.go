294d543e6b445a5377e2fff84f8d42aa59398147
Miguel Bolivar <MBolivar360@gmail.com>
2019-01-14 11:24:01 -0500

    Playing around with structs, creating a struct and 'objects' from it

---
+++ structs/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+}
+
+func main() {
+	var alex person
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson"
+	fmt.Printf("%+v", alex)
+}
