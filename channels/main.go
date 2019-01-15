7a4a1e411107d415dbb7b5cd2c7a00cb4937957d
Miguel Bolivar <MBolivar360@gmail.com>
2019-01-15 17:43:59 -0500

    Moving channel implementation to use a function literal

--- channels/main.go
+++ channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -20,8 +21,11 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	for {
-		go checkLink(<-c, c)
+	for l := range c {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
 	}
 
 }
